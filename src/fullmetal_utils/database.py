eb722d7219b7d5e5339321ff254ed0ab3fb609d1
Odos Matthews <odosmatthews@Odoss-Mac-mini.local>
2024-01-17 11:53:16 -0500

    add query method

--- src/fullmetal_utils/database.py
+++ src/fullmetal_utils/database.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from sqlalchemy import Engine, MetaData, Connection
+from typing import Iterator, Optional
+from sqlalchemy import Engine, MetaData, Connection, text
 
 
 class DataBase:
@@ -13,10 +13,21 @@ class DataBase:
         If you want to recreate a database from scratch
         (first clearing the existing database if it already exists)
         you can use the recreate=True argument:
+        db = Database(engine, recreate=True)
         """
         self.engine = engine
         if recreate:
             clear_database(engine, schema)
+
+    def query(self, sql: str, params) -> Iterator[dict]:
+        """
+        The db.query(sql) function executes a SQL query and returns an iterator
+        over Python dictionaries representing the resulting rows:
+        """
+        with self.engine.connect() as connection:
+            result = connection.execute(text(sql))
+            for row in result:
+                yield dict(row)
 
 
 def clear_database(
